a19477ac089263f34b04f3f7ba15ae77be14cb84
piterm6125 <115734952+piterm6125@users.noreply.github.com>
2023-02-04 16:10:06 +0100

    Update models.py

--- Produkty/models.py
+++ Produkty/models.py
@@ -6,6 +6,7 @@ class Produkty(models.Model):
     rocznik = models.DecimalField(max_digits=12, decimal_places=2)
     opis = models.TextField(blank=True)
     cena = models.DecimalField(max_digits=12, decimal_places=2)
+    #SDSADSADASDAS
 
     def __str__(self):
         return self.nazwa
