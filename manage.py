8fc60afed8745aa9318284ce79c955ce77e159ae
piterm6125 <115734952+piterm6125@users.noreply.github.com>
2023-02-04 16:17:59 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+#ssssssssssssssssssssssssssssssssssssssssssssssss
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dealer.settings')
