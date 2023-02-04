8526be5b81a34c88a60821ed2e3281271d330730
lukasha11 <84941658+lukasha11@users.noreply.github.com>
2023-02-04 15:58:09 +0100

    Add files via upload

---
+++ Produkty/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Produkty(models.Model):
+    nazwa = models.CharField(max_length=100)
+    rocznik = models.DecimalField(max_digits=12, decimal_places=2)
+    opis = models.TextField(blank=True)
+    cena = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return self.nazwa
